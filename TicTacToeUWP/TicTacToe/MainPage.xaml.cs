517d374a2ecd9e2c4f57895703316d46c1574ac2
asad98211 <asad98211@gmail.com>
2020-11-01 20:29:08 +0600

    gameview update and view model for game view added

--- TicTacToeUWP/TicTacToe/MainPage.xaml.cs
+++ TicTacToeUWP/TicTacToe/MainPage.xaml.cs
@@ -21,7 +21,7 @@ namespace TicTacToe
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class MainPage : Page
-    {
+    { 
         public MainPage()
         {
             this.InitializeComponent();
